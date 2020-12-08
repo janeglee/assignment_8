dd74aeecf47e3e73487eb370486d5cf24d0fc83a
janeglee <janegolee@gmail.com>
2020-12-07 23:30:27 -0800

    updates

--- script.js
+++ script.js
@@ -10,25 +10,42 @@ function drop(event) {
     event.preventDefault();
     var data = event.dataTransfer.getData("text");
     event.target.appendChild(document.getElementById(data));
+    console.log(data);
 }
 
 function randomImg () {
     const randomNumber = Math.floor(Math.random() * 4) + 1; 
-    console.log(randomNumber);
+    const imageID = null;
     if (randomNumber == 1) {
-        const cat = document.getElementById('cat');
-        return cat;
+        const img = document.createElement("img");
+        imageID = document.getElementById("cat");
+        img.src = document.getElementById("cat").src;
+        document.getElementById('randomImgBox').src = img.src;
     }
     else if (randomNumber == 2) {
-        const dog = document.getElementById('dog');
-        return dog;
+        const img = document.createElement("img");
+        img.src = document.getElementById("dog").src;
+        document.getElementById('randomImgBox').src = img.src;
     }
     else if (randomNumber == 3) {
-        cow = document.getElementById('cow');
-        return cow;
+        const img = document.createElement("img");
+        img.src = document.getElementById("cow").src;
+        document.getElementById('randomImgBox').src = img.src;
     }
     else if (randomNumber == 4) {
-        bird = document.getElementById('bird');
-        return bird;
+        const img = document.createElement("img");
+        img.src = document.getElementById("bird").src;
+        document.getElementById('randomImgBox').src = img.src;
+    }
+
+    if (imageID == data) {
+        //animate image according to action
+        //hmm "data" is same for animal and verb. need 2 sets, differentiate.
+        const action = document.getElementById
+    }
+    else {
+        //after delay, show "Try again!"
     }
 }
+
+//need to 
